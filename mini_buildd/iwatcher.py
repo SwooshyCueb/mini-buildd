d6ccc2ff96259af898af0624066a8f2773165652
Stephan Sürken <absurd@debian.org>
2010-04-29 19:05:48 +0200

    * mini_buildd/iwatcher.py: Also watch for MOVED_TO events (fixes auto backports).

--- mini_buildd/iwatcher.py
+++ mini_buildd/iwatcher.py
@@ -15,17 +15,23 @@ class IWatcher():
             self._queue = queue
             self._cfregex = re.compile("^.*\.changes$")
 
-        def process_IN_CREATE(self, event):
+        def process(self, event):
             log.debug("IN_CREATE event in incoming: %s" % str(event))
             if self._cfregex.match(event.pathname):
                 self._queue.put(event.pathname)
+
+        def process_IN_CREATE(self, event):
+            self.process(event)
+
+        def process_IN_MOVED_TO(self, event):
+            self.process(event)
 
     def __init__(self, queue, idir):
         log.info("Watching incoming: %s" % idir)
         self._handler = self.Handler(queue=queue)
         self._wm = pyinotify.WatchManager()
         self._notifier = pyinotify.Notifier(self._wm, default_proc_fun=self._handler)
-        self._wm.add_watch(idir, pyinotify.IN_CREATE)
+        self._wm.add_watch(idir, pyinotify.IN_CREATE | pyinotify.IN_MOVED_TO)
 
     def run(self):
         self._notifier.loop(daemonize=False)

f9f835d1c14c24778a0baa2117d38708f9f0fdc9
Stephan Sürken <absurd@debian.org>
2010-05-08 17:21:52 +0200

    * mini-buildd: Use python-daemon to daemonize correctly (PEP 3143).

--- mini_buildd/misc.py
+++ mini_buildd/misc.py
@@ -25,23 +25,3 @@ def run_cmd(cmd, no_act):
     l("Command '%s' run with retval %s" % (cmd, retval))
 
     return retval == 0
-
-def daemonize():
-    log.info("Daemonizing...")
-    if os.fork() == 0:
-        os.setsid()
-        if os.fork() != 0:
-            sys.exit(0)
-    else:
-        sys.exit(0)
-    sys.stdin.close()
-    sys.stdout.close()
-    sys.stderr.close()
-    os.close(0)
-    os.close(1)
-    os.close(2)
-    # unix file descriptor allocation ensures that the followin are fd 0,1,2
-    sys.stdin = open("/dev/null")
-    sys.stdout = open("/dev/null")
-    sys.stderr = open("/dev/null")
-    log.debug("Finished daemonizing (pid %s)" % (os.getpid(),))

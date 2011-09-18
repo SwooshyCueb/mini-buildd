f52d01b805fd8893830234a67e11164ea95d8310
Stephan Sürken <absurd@debian.org>
2011-09-18 09:51:16 +0200

    mini_buildd/admin.py: Obsoleted: import/export 08x models.

--- mini_buildd/admin.py
+++ mini_buildd/admin.py
@@ -12,7 +12,3 @@ admin.site.register(Suite)
 admin.site.register(Layout)
 admin.site.register(Distribution)
 admin.site.register(Repository)
-
-# 0.8.x compatibilty
-admin.site.register(Import08x)
-admin.site.register(Export08x)

c6c1c6f031d81eb515fc2840552b4369ae54c038
Stephan Sürken <absurd@debian.org>
2011-09-18 09:57:11 +0200

    mini_buildd/__init__.py: Add package import list to __init__.py.

---
+++ mini_buildd/__init__.py
@@ -0,0 +1,8 @@
+import mini_buildd.version
+import mini_buildd.misc
+
+from mini_buildd.options import opts
+from mini_buildd.log import log
+from mini_buildd.iwatcher import IWatcher
+from mini_buildd.installer import Installer
+from mini_buildd.webserver import WebServer, Django

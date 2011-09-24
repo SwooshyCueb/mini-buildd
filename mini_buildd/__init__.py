2a2da858d8e27fd114f907adee89f5f887dbfec6
Stephan Sürken <absurd@debian.org>
2011-09-24 19:59:39 +0200

    mini_buildd/webapp.py: Separate webserver && django app (webapp).

--- mini_buildd/__init__.py
+++ mini_buildd/__init__.py
@@ -4,6 +4,7 @@ import mini_buildd.misc
 from mini_buildd.options import opts, log
 from mini_buildd.iwatcher import IWatcher
 from mini_buildd.installer import Installer
-from mini_buildd.webserver import WebServer, Django
+from mini_buildd.webapp import WebApp
+from mini_buildd.webserver import WebServer
 
 import mini_buildd.compat08x
---
+++ mini_buildd/webapp.py
@@ -0,0 +1,62 @@
+# coding: utf-8
+import string
+import os
+
+from django.conf import settings
+import django.core.handlers.wsgi
+from django.core.management import call_command
+
+import mini_buildd
+
+class WebApp():
+    def __init__(self, debug=False):
+        mini_buildd.log.info("Configuring && generating django app...")
+        self._django = django.core.handlers.wsgi.WSGIHandler()
+        settings.configure(
+            DEBUG = debug,
+            TEMPLATE_DEBUG = debug,
+
+            # @todo: Seems this is needed for admin/doc.
+            SITE_ID = 1,
+
+            DATABASES =
+            {
+                'default':
+                    {
+                    'ENGINE': 'django.db.backends.sqlite3',
+                    'NAME': mini_buildd.opts.home + "/web/config.sqlite",
+                    }
+                },
+            TIME_ZONE = None,
+            USE_L10N = True,
+            SECRET_KEY = ")-%wqspscru#-9rl6u0sbbd*yn@$=ic^)-9c$+@@w898co2!7^",
+            ROOT_URLCONF = 'mini_buildd.root_urls',
+            INSTALLED_APPS = (
+                'django.contrib.auth',
+                'django.contrib.contenttypes',
+                'django.contrib.admin',
+                'django.contrib.sessions',
+                'django.contrib.admindocs',
+                'django_extensions',
+                'mini_buildd'
+                ))
+        self.syncdb()
+
+    def syncdb(self):
+        mini_buildd.log.info("Syncing database...")
+        call_command('syncdb', interactive=False)
+
+    def loaddata(self, f):
+        if os.path.splitext(f)[1] == ".conf":
+            mini_buildd.log.info("Try loading ad 08x.conf: {f}".format(f=f))
+            mini_buildd.compat08x.importConf(f)
+        else:
+            prefix = "" if f[0] == "/" else mini_buildd.opts.instdir + "/mini_buildd/fixtures/"
+            call_command('loaddata', prefix  + f)
+
+    def dumpdata(self, a):
+        mini_buildd.log.info("Dumping data for: {a}".format(a=a))
+        if a == "08x":
+            mini_buildd.compat08x.exportConf("/dev/stdout")
+        else:
+            call_command('dumpdata', a, indent=2, format='json')

56510fecc2eab384c06a4bde685b31543ff3e53b
Stephan Sürken <absurd@debian.org>
2011-09-25 10:48:46 +0200

    setup.py: Update for qualified imports.

--- setup.py
+++ setup.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
-from distutils.core import setup
-from debian.changelog import Changelog, Version
+import distutils.core
+import debian.changelog
 
 # This is a Debian native package, the version is in
 # debian/changelog and nowhere else. We automagically get the
 # version from there, and update the mini_buildd package's
 # __version__.py
-__version__ = str(Changelog(file=open("./debian/changelog", "rb")).version)
+__version__ = str(debian.changelog.Changelog(file=open("./debian/changelog", "rb")).version)
 with open("./mini_buildd/__version__.py", "wb") as version_py:
     version_py.write("# -*- coding: utf-8 -*-\n__version__ = '{version}'".format(version=__version__))
 print "I: Got version from changelog: {v}".format(v=__version__)
 
-setup(name = "mini-buildd",
-      version = __version__,
-      description = "Mini Debian build daemon",
-      author = "Stephan Sürken",
-      author_email = "absurd@debian.org",
-      scripts = ["mini-buildd"],
-      packages = ["mini_buildd"],
-      package_data = {"mini_buildd": ["templates/mini_buildd/*.html", "fixtures/*.json"]})
+distutils.core.setup(
+    name = "mini-buildd",
+    version = __version__,
+    description = "Mini Debian build daemon",
+    author = "Stephan Sürken",
+    author_email = "absurd@debian.org",
+    scripts = ["mini-buildd"],
+    packages = ["mini_buildd"],
+    package_data = {"mini_buildd": ["templates/mini_buildd/*.html", "fixtures/*.json"]})

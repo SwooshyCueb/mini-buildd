6b02ca573d6aa30179792452fba32e79dadf97cb
Stephan Sürken <absurd@debian.org>
2012-04-20 14:01:44 +0200

    Add support for CherryPy as internal web server, and make CherryPy default.

--- mini_buildd/webserver.py
+++ mini_buildd/webserver.py
@@ -1,7 +1,4 @@
 # coding: utf-8
-import wsgiref.simple_server
-import django.core.management
-
 import mini_buildd
 
 class WebServerBase(object):
@@ -11,23 +8,44 @@ class WebServerBase(object):
         self._port = int(bind[1])
         self._django = django
 
+    def run(self):
+        mini_buildd.log.info("Starting Web Server ({t}) on '{h}:{p}'.".format(t=self.__class__.__name__, h=self._host, p=self._port))
+        self._run()
+
+
+# CherryPy WSGI Web Server
+import cherrypy.wsgiserver
+class CherryPyWebServer(WebServerBase):
+    def __init__(self, django):
+        super(CherryPyWebServer, self).__init__(django)
+        self._httpd = cherrypy.wsgiserver.CherryPyWSGIServer((self._host, self._port), self._django)
+
+    def _run(self):
+        self._httpd.start()
+
+
+# Standard Library Reference WSGI Web Server
+import wsgiref.simple_server
 class WsgiRefWebServer(WebServerBase):
     def __init__(self, django):
         super(WsgiRefWebServer, self).__init__(django)
         self._httpd = wsgiref.simple_server.make_server(self._host, self._port, self._django)
 
-    def run(self):
-        mini_buildd.log.info("Starting WSGIRef web server: '{b}'.".format(b=mini_buildd.opts.bind))
+    def _run(self):
         self._httpd.serve_forever()
 
+
+# Django development Web Server
+import django.core.management
 class DjangoWebServer(WebServerBase):
     def __init__(self, django):
         super(DjangoWebServer, self).__init__(django)
 
-    def run(self):
-        mini_buildd.log.warn("Running django development server...")
+    def _run(self):
         django.core.management.call_command('runserver', self._host + ":" + str(self._port))
 
+
 # Set web server to be used
-WebServer = WsgiRefWebServer
+WebServer = CherryPyWebServer
+#WebServer = WsgiRefWebServer
 #WebServer = DjangoWebServer

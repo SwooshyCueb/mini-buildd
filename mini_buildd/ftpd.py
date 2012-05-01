fbf947c4779903491f105349664d2b2041968638
Stephan Sürken <absurd@debian.org>
2012-05-01 15:52:55 +0200

    incoming: Also queue *.buildrequest and *.buildresult files.

--- mini_buildd/ftpd.py
+++ mini_buildd/ftpd.py
@@ -17,19 +17,22 @@ pyftpdlib.ftpserver.logline  = lambda msg: log.debug(msg)
 pyftpdlib.ftpserver.logerror = lambda msg: log.error(msg)
 
 class FtpDHandler(pyftpdlib.ftpserver.FTPHandler):
-    def on_file_received(self, file):
-        os.chmod(file, stat.S_IRUSR | stat.S_IRGRP )
-        if self._mini_buildd_cfregex.match(file):
-            log.info("Queuing incoming changes file: %s" % file);
-            self._mini_buildd_queue.put(file)
+    def on_file_received(self, f):
+        # Make any incoming file read-only as soon as it arrives; avoids multiple user uploads of the same file
+        os.chmod(f, stat.S_IRUSR | stat.S_IRGRP)
+
+        if self._mini_buildd_queue_regex.match(f):
+            log.info("Queuing incoming file: {f}".format(f=f))
+            self._mini_buildd_queue.put(f)
         else:
-            log.debug("Skipping incoming file: %s" % file);
+            log.debug("Non-queue-able incoming file: {f}".format(f=f))
 
 
 class FtpD(pyftpdlib.ftpserver.FTPServer):
-    def __init__(self, bind, home, incoming, repositories, queue):
+    def __init__(self, bind, home, incoming, repositories, queue, queue_regex):
         self._bind = mini_buildd.misc.BindArgs(bind)
 
+        # @todo Arguably not the right place to create these dirs
         mini_buildd.misc.mkdirs(os.path.join(home, incoming))
         mini_buildd.misc.mkdirs(os.path.join(home, repositories))
 
@@ -41,7 +44,7 @@ class FtpD(pyftpdlib.ftpserver.FTPServer):
 
         handler.banner = "mini-buildd {v} ftp server ready (pyftpdlib {V}).".format(v=mini_buildd.__version__, V=pyftpdlib.ftpserver.__ver__)
         handler._mini_buildd_queue = queue
-        handler._mini_buildd_cfregex = re.compile("^.*\.changes$")
+        handler._mini_buildd_queue_regex = re.compile(queue_regex)
 
         pyftpdlib.ftpserver.FTPServer.__init__(self, self._bind.tuple, handler)
 

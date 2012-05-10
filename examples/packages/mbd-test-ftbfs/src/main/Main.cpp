df10091ebb53cd02b95b502f8f45c879b1bec31e
Stephan Sürken <absurd@debian.org>
2012-05-10 20:12:03 +0200

    test packages: Add "FTBFS" test package.

---
+++ examples/packages/mbd-test-ftbfs/src/main/Main.cpp
@@ -0,0 +1,11 @@
+// Local configuration
+#include "config.h"
+
+// C++ libraries
+#include <mbd-test-ftbfs/Lib.hpp>
+
+int main()
+{
+	Nicht schon wieder! Syntax ERROR!
+	MBD_TEST_CPP::mbd_test_cpp();
+}
---
+++ examples/packages/mbd-test-ftbfs/src/mbd-test-ftbfs/Lib.cpp
@@ -0,0 +1,16 @@
+// Project config
+#include "config.h"
+
+// Implementation
+#include "Lib.hpp"
+
+// STDC++
+#include <iostream>
+
+namespace MBD_TEST_CPP
+{
+	void mbd_test_cpp()
+	{
+		std::cout << "Test project mbd-test-ftbfs." << std::endl;
+	}
+}

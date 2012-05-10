bd9e2feb9a41ed82f75c284dff2f81c608325c64
Stephan Sürken <absurd@debian.org>
2012-05-10 19:51:05 +0200

    dispatcher: "Changes" refactor, buildrequest && buildresult file name fix ups.
    Roundtrip including buildresult uploads.

--- mini_buildd/dispatcher.py
+++ mini_buildd/dispatcher.py
@@ -14,83 +14,28 @@ import mini_buildd
 log = logging.getLogger(__name__)
 
 class Changes(debian.deb822.Changes):
-    def __init__(self, file_path, spool_dir):
+    BUILDREQUEST_RE = re.compile("^.+_mini-buildd-buildrequest_[^_]+.changes$")
+    BUILDRESULT_RE = re.compile("^.+_mini-buildd-buildresult_[^_]+.changes$")
+
+    def __init__(self, file_path):
         self._file_path = file_path
         self._file_name = os.path.basename(file_path)
-
-        if os.path.exists(file_path):
-            super(Changes, self).__init__(file(file_path))
-            self._spool_dir = os.path.join(spool_dir, self["Distribution"], self["Source"], self["Version"], self["Architecture"])
-        else:
-            super(Changes, self).__init__([])
-            self._spool_dir = None
-
-    def save(self, file_path=None):
-        file_path = self._file_path if file_path == None else file_path
-
-        log.info("Save {f}".format(f=file_path))
-        self.dump(fd=open(file_path, "w+"))
-        self._file_path = file_path
-        self._file_name = os.path.basename(file_path)
-
-    def get_file_name(self, new_ext=None):
-        if new_ext == None:
-            return self._file_name
-        else:
-            return os.path.splitext(self._file_name)[0] + new_ext
-
-class Buildrequest(Changes):
-    def __init__(self, file_path, spool_dir):
-        super(Buildrequest, self).__init__(file_path, spool_dir)
-
-        # Create spool directory; this may already exist
-        if self._spool_dir:
-            mini_buildd.misc.mkdirs(self._spool_dir)
-
         self._tar_path = file_path.rpartition("_")[0]
+        super(Changes, self).__init__(file(file_path) if os.path.exists(file_path) else [])
+        # Be sure base dir is always available
+        mini_buildd.misc.mkdirs(os.path.dirname(file_path))
 
-    def upload(self, host="localhost", port=8067):
-        ftp = ftplib.FTP()
-        ftp.connect(host, port)
-        ftp.login()
-        ftp.cwd("/incoming")
-        ftp.storbinary("STOR {f}".format(f=self._file_name), open(self._file_path))
 
-        try:
-            ftp.size(os.path.basename(self._tar_path))
-            log.info("Already uploaded to this host: '{f}'...". format(f=self._tar_path))
-        except:
-            ftp.storbinary("STOR {f}".format(f=os.path.basename(self._tar_path)), open(self._tar_path))
+    def is_buildrequest(self):
+        return self.BUILDREQUEST_RE.match(self._file_name)
 
-    def unpack(self):
-        tar = tarfile.open(self._tar_path, "r")
-        tar.extractall(path=self._spool_dir)
-        tar.close()
-        return self._spool_dir
+    def is_buildresult(self):
+        return self.BUILDRESULT_RE.match(self._file_name)
 
-class SourceChanges(Changes):
-    def __init__(self, file_path, spool_dir):
-        super(SourceChanges, self).__init__(file_path, spool_dir)
-        self._base_spool_dir = spool_dir
-
-        # @todo: If uploaded input, check it does not exist yet
-        mini_buildd.misc.mkdirs(self._spool_dir)
-
-        self._tar_path = os.path.join(self._spool_dir, self._file_name) + ".tar"
-        tar = tarfile.open(self._tar_path, "w")
-        try:
-            tar_add = lambda f: tar.add(f, arcname=os.path.basename(f))
-            tar_add(self._file_path)
-            for f in self.get_files():
-                tar_add(os.path.join(os.path.dirname(self._file_path), f["name"]))
-        finally:
-            tar.close()
-
-    def get_files(self):
-        try:
-            return self["Files"]
-        except:
-            return {}
+    def get_type(self):
+        print self._file_name.rsplit("_")
+        print self._file_name.rpartition("_")
+        return os.path.splitext(self._file_name)[1]
 
     def get_repository(self):
         from mini_buildd.models import Repository
@@ -102,25 +47,71 @@ class SourceChanges(Changes):
         # @todo Check that base dist is really supported by this repo
         return r
 
-    def gen_build_requests(self):
+    def get_spool_dir(self, base_dir):
+        return os.path.join(base_dir, self["Distribution"], self["Source"], self["Version"], self["Buildrequest-Architecture"])
+
+    def get_pkg_id(self):
+        return "{s}_{v}".format(s=self["Source"], v=self["Version"])
+
+    def get_files(self):
+        return self["Files"] if "Files" in self else []
+
+    def save(self, file_path=None):
+        file_path = self._file_path if file_path == None else file_path
+
+        log.info("Save {f}".format(f=file_path))
+        self.dump(fd=open(file_path, "w+"))
+        self._file_path = file_path
+        self._file_name = os.path.basename(file_path)
+
+    def upload(self, host="localhost", port=8067):
+        ftp = ftplib.FTP()
+        ftp.connect(host, port)
+        ftp.login()
+        ftp.cwd("/incoming")
+        for fd in self.get_files() + [ {"name": self._file_name} ]:
+            f = fd["name"]
+            log.debug("FTP-Upload: '{f}'". format(f=f))
+            try:
+                ftp.list()
+                ftp.size(f)
+                log.warn("Already uploaded to this host: '{f}'...". format(f=f))
+            except:
+                ftp.storbinary("STOR {f}".format(f=f), open(os.path.join(os.path.dirname(self._file_path), f)))
+
+    def tar(self, tar_path):
+        tar = tarfile.open(tar_path, "w")
+        try:
+            tar_add = lambda f: tar.add(f, arcname=os.path.basename(f))
+            tar_add(self._file_path)
+            for f in self.get_files():
+                tar_add(os.path.join(os.path.dirname(self._file_path), f["name"]))
+        finally:
+            tar.close()
+
+    def gen_buildrequests(self, base_dir):
         # Build buildrequest files for all archs
         br_list = []
         r = self.get_repository()
         for a in r.archs.all():
-            brf = "{b}_{a}.buildrequest".format(b=self._tar_path, a=a.arch)
-            br = Buildrequest(brf, self._base_spool_dir)
-            # @todo Add all build information from repository
+            br = Changes(os.path.join(base_dir, "{b}_mini-buildd-buildrequest_{a}.changes".format(b=self.get_pkg_id(), a=a.arch)))
             for v in ["Distribution", "Source", "Version"]:
                 br[v] = self[v]
-            br["Base-Distribution"] = br["Distribution"].split("-")[0]
-            br["Architecture"] = a.arch
+
+            # Generate tar from original changes
+            self.tar(tar_path=br._file_path + ".tar")
+            # [ "md5sum", "size", "section", "priority", "name" ]
+            br["Files"] = [{"md5sum": "FIXME", "size": "FIXME", "section": "mini-buildd-buildrequest", "priority": "FIXME", "name": br._file_name + ".tar"}]
+
+            br["Buildrequest-Base-Distribution"] = br["Distribution"].split("-")[0]
+            br["Buildrequest-Architecture"] = a.arch
             if a == r.arch_all:
-                br["Arch-All"] = "Yes"
-            br["Build-Dep-Resolver"] = r.build_dep_resolver
-            br["Apt-Allow-Unauthenticated"] = "1" if r.apt_allow_unauthenticated else "0"
+                br["Buildrequest-Arch-All"] = "Yes"
+            br["Buildrequest-Build-Dep-Resolver"] = r.build_dep_resolver
+            br["Buildrequest-Apt-Allow-Unauthenticated"] = "1" if r.apt_allow_unauthenticated else "0"
             if r.lintian_mode != "disabled":
                 # Generate lintian options
-                br["Run-Lintian"] = {"never-fail": "", "fail-on-error": "", "fail-on-warning": "--fail-on-warning"}[r.lintian_mode] + " " + r.lintian_extra_options
+                br["Buildrequest-Run-Lintian"] = {"never-fail": "", "fail-on-error": "", "fail-on-warning": "--fail-on-warning"}[r.lintian_mode] + " " + r.lintian_extra_options
 
             br.save()
             br_list.append(br)
@@ -129,33 +120,36 @@ class SourceChanges(Changes):
 
 
 class Build():
-    def __init__(self, spool_dir, f):
-        self._br = Buildrequest(f, spool_dir)
+    def __init__(self, spool_dir, br):
+        self._br = br
         self._spool_dir = spool_dir
 
-    def results_from_buildlog(self, fn):
+    def results_from_buildlog(self, fn, changes):
         regex = re.compile("^[a-zA-Z0-9-]+: [^ ]+$")
         with open(fn) as f:
             for l in f:
                 if regex.match(l):
                     log.debug("Build log line detected as build status: {l}".format(l=l.strip()))
                     s = l.split(":")
-                    self._br["Buildresult-Sbuild-" + s[0]] = s[1].strip()
+                    changes["Buildresult-Sbuild-" + s[0]] = s[1].strip()
 
     def run(self):
         # @todo Caveat: Create sbuild's internal key if needed. This should go somewhere else.
         if not os.path.exists("/var/lib/sbuild/apt-keys/sbuild-key.pub"):
             mini_buildd.misc.run_cmd("sbuild-update --keygen")
 
-        pkg_info = "{s}-{v}:{a}".format(s=self._br["Source"], v=self._br["Version"], a=self._br["Architecture"])
+        pkg_info = "{s}-{v}:{a}".format(s=self._br["Source"], v=self._br["Version"], a=self._br["Buildrequest-Architecture"])
 
-        path = self._br.unpack()
+        path = self._br.get_spool_dir(self._spool_dir)
 
-        # @todo
-        #if DEB_BUILD_OPTIONS="${mbdParseArch_debopts}"
+        tar = tarfile.open(self._br._file_path + ".tar", "r")
+        try:
+            tar.extractall(path=path)
+        finally:
+            tar.close()
 
-        # Run sbuild. Notes:
-        # * DEB_BUILD_OPTIONS are configured per build host.
+        # @todo DEB_BUILD_OPTIONS
+
         # Generate .sbuildrc for this run (not all is configurable via switches).
         open(os.path.join(path, ".sbuildrc"), 'w').write("""
 # Set "user" mode explicitely (already default).  Means the
@@ -180,33 +174,33 @@ $pgp_options = ['-us', '-k Mini-Buildd Automatic Signing Key'];
 
 # don't remove this, Perl needs it:
 1;
-""".format(apt_allow_unauthenticated=self._br["Apt-Allow-Unauthenticated"]))
+""".format(apt_allow_unauthenticated=self._br["Buildrequest-Apt-Allow-Unauthenticated"]))
 
         env = os.environ
         env["HOME"] = path
 
         sbuild_cmd = ["sbuild",
                       "--dist={0}".format(self._br["Distribution"]),
-                      "--arch={0}".format(self._br["Architecture"]),
-                      "--chroot=mini-buildd-{d}-{a}".format(d=self._br["Base-Distribution"], a=self._br["Architecture"]),
-                      "--build-dep-resolver={r}".format(r=self._br["Build-Dep-Resolver"]),
+                      "--arch={0}".format(self._br["Buildrequest-Architecture"]),
+                      "--chroot=mini-buildd-{d}-{a}".format(d=self._br["Buildrequest-Base-Distribution"], a=self._br["Buildrequest-Architecture"]),
+                      "--build-dep-resolver={r}".format(r=self._br["Buildrequest-Build-Dep-Resolver"]),
                       "--verbose", "--nolog", "--log-external-command-output", "--log-external-command-error"]
 
-        if "Arch-All" in self._br:
+        if "Buildrequest-Arch-All" in self._br:
             sbuild_cmd.append("--arch-all")
 
         # @ todo lintian opt-in, repository options
         if "Run-Lintian" in self._br:
             sbuild_cmd.append("--run-lintian")
             sbuild_cmd.append("--lintian-opts=--suppress-tags=bad-distribution-in-changes-file")
-            sbuild_cmd.append("--lintian-opts={o}".format(o=self._br["Run-Lintian"]))
+            sbuild_cmd.append("--lintian-opts={o}".format(o=self._br["Buildrequest-Run-Lintian"]))
 
         if mini_buildd.globals.DEBUG:
-            sbuild_cmd.append("--debug")
+            sbuild_cmd.append("--verbose")
 
         sbuild_cmd.append("{s}_{v}.dsc".format(s=self._br["Source"], v=self._br["Version"]))
 
-        buildlog = os.path.join(path, "{s}_{v}_{a}.buildlog".format(s=self._br["Source"], v=self._br["Version"], a=self._br["Architecture"]))
+        buildlog = os.path.join(path, "{s}_{v}_{a}.buildlog".format(s=self._br["Source"], v=self._br["Version"], a=self._br["Buildrequest-Architecture"]))
         log.info("{p}: Starting sbuild".format(p=pkg_info))
         log.debug("{p}: Sbuild options: {c}".format(p=pkg_info, c=sbuild_cmd))
         with open(buildlog, "w") as l:
@@ -214,23 +208,30 @@ $pgp_options = ['-us', '-k Mini-Buildd Automatic Signing Key'];
                                      cwd=path, env=env,
                                      stdout=l, stderr=subprocess.STDOUT)
 
+        res = Changes(os.path.join(path,
+                                   "{s}_{v}_mini-buildd-buildresult_{a}.changes".
+                                   format(s=self._br["Source"], v=self._br["Version"], a=self._br["Buildrequest-Architecture"])))
+        for v in ["Distribution", "Source", "Version"]:
+            res[v] = self._br[v]
+
         # Add build results to build request object
-        self._br["Buildresult-Retval"] = retval
-        self.results_from_buildlog(buildlog)
+        res["Buildresult-Retval"] = retval
+        self.results_from_buildlog(buildlog, res)
 
-        log.info("{p}: Sbuild finished: Retval={r}, Status={s}".format(p=pkg_info, r=retval, s=self._br["Buildresult-Sbuild-Status"]))
-
-        if self._br["Buildresult-Sbuild-Status"] != "skipped":
-            build_changes = SourceChanges(os.path.join(
+        log.info("{p}: Sbuild finished: Retval={r}, Status={s}".format(p=pkg_info, r=retval, s=res["Buildresult-Sbuild-Status"]))
+        res["Files"] = []
+        if res["Buildresult-Sbuild-Status"] != "skipped":
+            build_changes = Changes(os.path.join(
                     path,
                     "{s}_{v}_{a}.changes".
-                    format(s=self._br["Source"], v=self._br["Version"], a=self._br["Architecture"])),
-                                          self._spool_dir)
+                    format(s=self._br["Source"], v=self._br["Version"], a=self._br["Buildrequest-Architecture"])))
+            build_changes.tar(tar_path=res._file_path + ".tar")
+            res["Files"].append({"md5sum": "FIXME", "size": "FIXME", "section": "mini-buildd-buildresult", "priority": "FIXME", "name": res._file_name + ".tar"})
 
-        self._br.save(os.path.join(
-                path,
-                "{s}_{v}_{a}.changes.tar_{a}.buildresult".
-                format(s=self._br["Source"], v=self._br["Version"], a=self._br["Architecture"])))
+        res["Files"].append({"md5sum": "FIXME", "size": "FIXME", "section": "mini-buildd-buildresult", "priority": "FIXME", "name": os.path.basename(buildlog)})
+
+        res.save()
+        res.upload()
 
 
 class Builder():
@@ -257,19 +258,14 @@ class Dispatcher():
     def run(self):
         mini_buildd.misc.start_thread(self._builder)
         while True:
-            f = self._incoming_queue.get()
-            ext = os.path.splitext(f)[1]
-            if ext == ".changes":
-                # User upload
-                c = SourceChanges(f, os.path.join(self._spool_dir, "repositories"))
-                for br in c.gen_build_requests():
-                    br.upload()
-
-            elif ext == ".buildrequest":
-                self._build_queue.put(f)
-            elif ext == ".buildresult":
-                log.info("STUB: build result: '{f}'...".format(f=f))
+            c = Changes(self._incoming_queue.get())
+            if c.is_buildrequest():
+                self._build_queue.put(c)
+            elif c.is_buildresult():
+                log.info("STUB: build result: '{f}'...".format(f=c._file_name))
             else:
-                raise Exception("Internal error: Wrong incoming file {f}".format(f=f))
+                # User upload
+                for br in c.gen_buildrequests(os.path.join(self._spool_dir, "repositories")):
+                    br.upload()
 
             self._incoming_queue.task_done()

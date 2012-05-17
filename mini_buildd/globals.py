05b41c114b933a596f5d5b93d4a7fe962ec3284a
Stephan Sürken <absurd@debian.org>
2012-05-17 08:50:11 +0200

    globals: Add set of global directory paths.

--- mini_buildd/globals.py
+++ mini_buildd/globals.py
@@ -1,1 +1,12 @@
 DEBUG = False
+
+# Global directory paths
+HOME_DIR = None
+
+INCOMING_DIR = None
+REPOSITORIES_DIR = None
+
+SPOOL_DIR = None
+BUILDS_DIR = None
+LOGS_DIR = None
+CHROOTS_DIR = None

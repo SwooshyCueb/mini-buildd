19e84881298f3458696c4789447bea929f5eb5d5
Stephan Sürken <absurd@debian.org>
2012-05-23 16:33:29 +0200

    graphviz: dont break if praphviz is not installed.
    This should go somewhere to the manual anyway, eventually.

--- mini_buildd/views.py
+++ mini_buildd/views.py
@@ -8,7 +8,10 @@ import django.http
 log = logging.getLogger(__name__)
 
 def graph_models(request):
-    with tempfile.NamedTemporaryFile(suffix=".png") as img:
-        log.info("Generating graphical model view using tmpfile=" + img.name)
-        django.core.management.call_command('graph_models', 'mini_buildd', outputfile=img.name)
-        return django.http.HttpResponse(img.read(), mimetype="image/png")
+    try:
+        with tempfile.NamedTemporaryFile(suffix=".png") as img:
+            log.info("Generating graphical model view using tmpfile=" + img.name)
+            django.core.management.call_command('graph_models', 'mini_buildd', outputfile=img.name)
+            return django.http.HttpResponse(img.read(), mimetype="image/png")
+    except:
+        return django.http.HttpResponse("Can't produce graphic (python-pygraphviz not installed?)", mimetype="text/plain")

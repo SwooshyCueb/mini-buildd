627edc1b29dc964b483690b84975d06091c118b2
Stephan Sürken <absurd@debian.org>
2012-05-31 18:15:25 +0200

    gnupg.py: Fix up and simplify with new misc.call().

--- mini_buildd/gnupg.py
+++ mini_buildd/gnupg.py
@@ -1,14 +1,8 @@
 # -*- coding: utf-8 -*-
-import StringIO
 import os
-import stat
-import re
-import datetime
 import tempfile
 import socket
-import subprocess
 import logging
-import signal
 
 from mini_buildd import globals, misc
 
@@ -19,54 +13,33 @@ class GnuPG():
         self.gpg_cmd = ["gpg",
                         "--homedir={h}".format(h=os.path.join(globals.HOME_DIR, ".gnupg")),
                         "--batch"]
-        self.template = template
-
-    def prepare(self):
-        if not self.get_pub_key():
-            with tempfile.NamedTemporaryFile() as l:
-                with tempfile.NamedTemporaryFile() as tpl:
-                    tpl.write("""\
+        self.template = tempfile.TemporaryFile()
+        self.template.write("""\
 {t}
 Name-Real: Mini Buildd Archive Key
 Name-Email: mini-buildd@{h}
-""".format(t=self.template, h=socket.getfqdn()))
-                    tpl.seek(0)
-                    try:
-                        subprocess.check_call(self.gpg_cmd + ["--gen-key"],
-                                              stdin=tpl,
-                                              stdout=l, stderr=subprocess.STDOUT)
-                    except:
-                        l.seek(0)
-                        log.error(l.read())
-                        raise
+""".format(t=template, h=socket.getfqdn()))
 
-    def purge(self):
-        log.error(self.__name__ + ".purge(): STUB")
+    def prepare(self):
+        if not self.get_pub_key():
+            self.template.seek(0)
+            misc.call(self.gpg_cmd + ["--gen-key"], stdin=self.template)
 
     def get_pub_key(self):
-        result = ""
-        with tempfile.NamedTemporaryFile() as stderr:
-            with tempfile.NamedTemporaryFile() as stdout:
-                try:
-                    subprocess.check_call(self.gpg_cmd + ["--armor", "--export={i}".format(i="mini-buildd")],
-                                          stdin=stderr,
-                                          stdout=stdout)
-                    stdout.seek(0)
-                    return stdout.read()
-                except:
-                    stderr.seek(0)
-                    log.error(stderr.read())
-                    raise
+        return misc.call(self.gpg_cmd + ["--armor", "--export={i}".format(i="mini-buildd")])
+
+    def get_fingerprint(self):
+        return misc.call(self.gpg_cmd + ["--armor", "--fingerprint={i}".format(i="mini-buildd")])
 
 if __name__ == "__main__":
-    print __name__
-    log.addHandler(logging.StreamHandler())
+    #misc.setup_test_logging()
 
     globals.HOME_DIR = "/tmp/gnupgtest"
+    misc.mkdirs(globals.HOME_DIR)
     gnupg = GnuPG("""\
 Key-Type: DSA
 Key-Length: 1024
 Expire-Date: 0""")
     gnupg.prepare()
-
     print gnupg.get_pub_key()
+    print gnupg.get_fingerprint()

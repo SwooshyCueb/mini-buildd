47c4021c4c23020fe2fc29c3194a7ecc7af2841f
Stephan Sürken <absurd@debian.org>
2012-05-31 13:42:29 +0200

    misc.py: Obsoleting ye ole run_cmd helper.

--- mini_buildd/misc.py
+++ mini_buildd/misc.py
@@ -72,21 +72,6 @@ def mkdirs(path):
         else:
             log.debug("Directory already exists, ignoring; {d}".format(d=path))
 
-def run_cmd(cmd):
-    # Run command, keep output
-    output = tempfile.TemporaryFile()
-    log.info("Running system command: '%s'" % cmd)
-    retval = subprocess.call([cmd], shell=True, stdout=output, stderr=subprocess.STDOUT)
-
-    # Log command output
-    l = log.debug if (retval == 0) else log.error
-    output.seek(0)
-    for line in output:
-        l("Command output: %s" % line.replace("\n", ""))
-
-    if retval != 0:
-        raise Exception("Command '{c}' failed with retval {r}".format(c=cmd, r=retval))
-
 def call(args, run_as_root=False, value_on_error=None, log_output=True, **kwargs):
     if run_as_root:
         args = ["sudo"] + args

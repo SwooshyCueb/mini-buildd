7225dd9f42298eed8f06dd7480cd34723bf52d04
Stephan Sürken <absurd@debian.org>
2012-06-07 15:28:55 +0200

    mini-buildd: Upgrade manager to start_as_thread().

--- mini_buildd/manager.py
+++ mini_buildd/manager.py
@@ -35,35 +35,35 @@ Expire-Date: 0""")
         if Manager.objects.count() > 0 and self.id != Manager.objects.get().id:
             raise django.core.exceptions.ValidationError("You can only create one Manager instance!")
 
-    def mbd_prepare(self):
-        self.gnupg.prepare()
-
-    def run(self, incoming_queue, build_queue):
-        log.info("Preparing {d}".format(d=self))
-        self.mbd_prepare()
-
-        log.info("Starting {d}".format(d=self))
-        while True:
-            event = incoming_queue.get()
-            if event == "SHUTDOWN":
-                break
-
-            c = changes.Changes(event)
-            r = c.get_repository()
-            if c.is_buildrequest():
-                log.info("{p}: Got build request for {r}".format(p=c.get_pkg_id(), r=r.id))
-                build_queue.put(event)
-            elif c.is_buildresult():
-                log.info("{p}: Got build result for {r}".format(p=c.get_pkg_id(), r=r.id))
-                c.untar(path=r.mbd_get_incoming_path())
-                r._reprepro.processincoming()
-            else:
-                log.info("{p}: Got user upload for {r}".format(p=c.get_pkg_id(), r=r.id))
-                for br in c.gen_buildrequests():
-                    br.upload()
-
-            incoming_queue.task_done()
-
-        log.info("Stopped {d}".format(d=self))
-
 django.contrib.admin.site.register(Manager)
+
+def run(incoming_queue, build_queue):
+    manager, created = Manager.objects.get_or_create(id=1)
+
+    # todo: Own GnuPG model
+    log.info("Preparing {d}".format(d=manager))
+    manager.gnupg.prepare()
+
+    log.info("Starting {d}".format(d=manager))
+    while True:
+        event = incoming_queue.get()
+        if event == "SHUTDOWN":
+            break
+
+        c = changes.Changes(event)
+        r = c.get_repository()
+        if c.is_buildrequest():
+            log.info("{p}: Got build request for {r}".format(p=c.get_pkg_id(), r=r.id))
+            build_queue.put(event)
+        elif c.is_buildresult():
+            log.info("{p}: Got build result for {r}".format(p=c.get_pkg_id(), r=r.id))
+            c.untar(path=r.mbd_get_incoming_path())
+            r._reprepro.processincoming()
+        else:
+            log.info("{p}: Got user upload for {r}".format(p=c.get_pkg_id(), r=r.id))
+            for br in c.gen_buildrequests():
+                br.upload()
+
+        incoming_queue.task_done()
+
+    log.info("Stopped {d}".format(d=manager))

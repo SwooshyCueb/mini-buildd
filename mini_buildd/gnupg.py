3a50d29ef195b76386625f2514babfb879939300
Stephan Sürken <absurd@debian.org>
2012-07-04 00:15:34 +0200

    gnupg.py: Add features: add_pub_key, sign, verify.

--- mini_buildd/gnupg.py
+++ mini_buildd/gnupg.py
@@ -13,6 +13,12 @@ class BaseGnuPG(object):
                         "--homedir={h}".format(h=home),
                         "--batch"]
 
+    def gen_secret_key(self, template):
+        with tempfile.TemporaryFile() as t:
+            t.write(template)
+            t.seek(0)
+            misc.call(self.gpg_cmd + ["--gen-key"], stdin=t)
+
     def get_pub_key(self, id):
         return misc.call(self.gpg_cmd + ["--armor", "--export={i}".format(i=id)])
 
@@ -22,32 +28,41 @@ class BaseGnuPG(object):
     def recv_key(self, keyserver, id):
         return misc.call(self.gpg_cmd + ["--armor", "--keyserver={ks}".format(ks=keyserver), "--recv-keys", id])
 
-    def _purge_home(self):
-        if os.path.exists(self.home):
-            shutil.rmtree(self.home)
+    def add_pub_key(self, key):
+        with tempfile.TemporaryFile() as t:
+            t.write(key)
+            t.seek(0)
+            misc.call(self.gpg_cmd + ["--import"], stdin=t)
+
+    def verify(self, file):
+        misc.call(self.gpg_cmd + ["--verify", file])
+
+    def sign(self, file, id):
+        signed_file = file + ".signed"
+        misc.call(self.gpg_cmd + ["--armor", "--textmode", "--clearsign", "--local-user={i}".format(i=id), "--output={f}".format(f=signed_file), file])
+        os.rename(signed_file, file)
 
 class GnuPG(BaseGnuPG):
     def __init__(self, template):
         super(GnuPG, self).__init__(home=os.path.join(setup.HOME_DIR, ".gnupg"))
-        self.template = tempfile.TemporaryFile()
-        self.template.write("""\
+        self.template = """\
 {t}
 Name-Real: Mini Buildd Archive Key
 Name-Email: mini-buildd@{h}
-""".format(t=template, h=socket.getfqdn()))
+""".format(t=template, h=socket.getfqdn())
 
     def prepare(self, r):
         if not self.get_pub_key():
             msg_info(r, "Generating GnuPG secret key (this might take some time)...")
-            self.template.seek(0)
-            misc.call(self.gpg_cmd + ["--gen-key"], stdin=self.template)
+            self.gen_secret_key(self.template)
             msg_info(r, "New GnuPG secret key prepared...")
         else:
             msg_info(r, "GnuPG key already prepared...")
 
     def unprepare(self, r):
-        self._purge_home()
-        msg_info(r, "GnuPG setup removed: {h}".format(h=self.home))
+        if os.path.exists(self.home):
+            shutil.rmtree(self.home)
+            msg_info(r, "GnuPG setup removed: {h}".format(h=self.home))
 
     def get_pub_key(self):
         return super(GnuPG, self).get_pub_key("mini-buildd")
@@ -56,26 +71,27 @@ Name-Email: mini-buildd@{h}
         return super(GnuPG, self).get_fingerprint("mini-buildd")
 
 class TmpGnuPG(BaseGnuPG):
+    """
+    >>> gnupg = TmpGnuPG()
+    >>> gnupg.gen_secret_key("Key-Type: DSA\\nKey-Length: 1024\\nName-Email: test@key.org")
+    >>> t = tempfile.NamedTemporaryFile()
+    >>> t.write("A test file")
+    >>> gnupg.sign(file=t.name, id="test@key.org")
+    >>> gnupg.verify(t.name)
+    >>> pub_key = gnupg.get_pub_key(id="test@key.org")
+    >>> tgnupg = TmpGnuPG()
+    >>> tgnupg.add_pub_key(pub_key)
+    >>> tgnupg.verify(t.name)
+    """
     def __init__(self):
         super(TmpGnuPG, self).__init__(home=tempfile.mkdtemp())
 
     def __del__(self):
-        self._purge_home()
+        import shutil
+        shutil.rmtree(self.home)
 
 if __name__ == "__main__":
     misc.setup_test_logging()
 
-#    setup.HOME_DIR = "/tmp/gnupgtest"
-#    misc.mkdirs(setup.HOME_DIR)
-#    gnupg = GnuPG("""\
-#Key-Type: DSA
-#Key-Length: 1024
-#Expire-Date: 0""")
-#    gnupg.prepare()
-#    print gnupg.get_pub_key()
-#    print gnupg.get_fingerprint()
-
-    gnupg = TmpGnuPG()
-    gnupg.recv_key(keyserver="subkeys.pgp.net", id="473041FA")
-    print gnupg.get_pub_key(id="473041FA")
-    print gnupg.get_fingerprint(id="473041FA")
+    import doctest
+    doctest.testmod()

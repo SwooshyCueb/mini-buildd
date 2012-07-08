f7cfef38b7087a1901cdf08b07545f2f365f57d2
Stephan Sürken <absurd@debian.org>
2012-07-08 17:01:19 +0200

    doc/django_settings.py: Add auth and contenttypes admin apps -- needed ate least for sphinx under sid.

--- doc/django_settings.py
+++ doc/django_settings.py
@@ -1,5 +1,7 @@
 # Dummy; needed so django does not freak out on doc building
 INSTALLED_APPS = (
     'django_extensions',
+    'django.contrib.auth',
+    'django.contrib.contenttypes',
     'mini_buildd'
     )

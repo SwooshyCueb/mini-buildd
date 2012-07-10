a0a1ea6c0e953b651d1f1cb8902894f61ece5da7
Gerhard A. Dittes <gerhard.dittes@1und1.de>
2012-07-10 12:44:44 +0000

    mini_buildd/templatetags/index.py: Check for existing repositories before trying to list them.

--- mini_buildd/templatetags/index.py
+++ mini_buildd/templatetags/index.py
@@ -14,8 +14,13 @@ def mbd_status():
 
 @register.simple_tag
 def mbd_repository_list():
-    ret = "<ul>"
-    for repo in mini_buildd.models.Repository.objects.all():
-        ret += '<li><a href="repositories/' + repo.identity + '">' + repo.identity + '</a></li>'
-    ret += "</ul>"
+    ret = ""
+    if mini_buildd.models.Repository.objects.all().count() > 0:
+        ret += "<ul>"
+        for repo in mini_buildd.models.Repository.objects.all():
+            ret += '<li><a href="repositories/' + repo.identity + '">' + repo.identity + '</a></li>'
+        ret += "</ul>"
+    else:
+        ret += "<p>No repositories configured.</p>"
+
     return ret

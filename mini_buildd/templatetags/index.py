7fdf27d77634ef5b436a1a7c5890931894f78c1d
Gerhard A. Dittes <gerhard.dittes@1und1.de>
2012-07-11 15:18:35 +0000

    Website: Add package search functionality; Improve page footer (and overall appearance).

--- mini_buildd/templatetags/index.py
+++ mini_buildd/templatetags/index.py
@@ -2,7 +2,6 @@
 import django
 
 import mini_buildd.daemon
-import mini_buildd.models
 
 register = django.template.Library()
 
@@ -10,17 +9,3 @@ register = django.template.Library()
 @register.simple_tag
 def mbd_status():
     return mini_buildd.daemon.get().status_as_html()
-
-
-@register.simple_tag
-def mbd_repository_list():
-    ret = ""
-    if mini_buildd.models.Repository.objects.all().count() > 0:
-        ret += "<ul>"
-        for repo in mini_buildd.models.Repository.objects.all():
-            ret += '<li><a href="repositories/' + repo.identity + '">' + repo.identity + '</a></li>'
-        ret += "</ul>"
-    else:
-        ret += "<p>No repositories configured.</p>"
-
-    return ret
--- mini_buildd/templatetags/repository.py
+++ mini_buildd/templatetags/repository.py
@@ -1,7 +1,21 @@
 # -*- coding: utf-8 -*-
 import django
+import mini_buildd.models
 
 register = django.template.Library()
+
+@register.simple_tag
+def repository_list_all():
+    ret = ""
+    if mini_buildd.models.Repository.objects.all().count() > 0:
+        ret += "<ul>"
+        for repo in mini_buildd.models.Repository.objects.all():
+            ret += '<li><a href="/mini_buildd/repositories/' + repo.identity + '">' + repo.identity + '</a></li>'
+        ret += "</ul>"
+    else:
+        ret += "<p>No repositories configured.</p>"
+
+    return ret
 
 
 @register.simple_tag

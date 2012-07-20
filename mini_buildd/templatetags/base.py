8d0f7117ccf49bfa69e47cab649f8348df7376bb
Gerhard A. Dittes <gerhard.dittes@1und1.de>
2012-07-20 15:14:38 +0000

    Website: Reduce count of "template-tags" files -- use only "base.py" from now on.

--- mini_buildd/templatetags/base.py
+++ mini_buildd/templatetags/base.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 import django
-
 import mini_buildd
+
 
 register = django.template.Library()
 
@@ -9,3 +9,33 @@ register = django.template.Library()
 @register.simple_tag
 def mbd_version():
     return mini_buildd.__version__
+
+
+@register.simple_tag
+def mbd_status():
+    return mini_buildd.daemon.get().status_as_html()
+
+
+@register.simple_tag
+def repository_dist(repository, dist, suite):
+    return repository.mbd_get_dist(dist, suite)
+
+
+@register.simple_tag
+def repository_desc(repository, dist, suite):
+    return repository.mbd_get_desc(dist, suite)
+
+
+@register.simple_tag
+def repository_apt_line(repository, dist, suite):
+    return repository.mbd_get_apt_line(dist, suite)
+
+
+@register.simple_tag
+def repository_sources(repository, dist, suite):
+    return repository.mbd_get_sources(dist, suite)
+
+
+@register.simple_tag
+def repository_mandatory_version(repository, dist, suite):
+    return repository.layout.mbd_get_mandatory_version_regex(repository, dist, suite)

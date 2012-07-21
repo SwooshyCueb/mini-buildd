20c65ce2762f3804df498532a5dae2bfa13c801e
Stephan Sürken <absurd@debian.org>
2012-07-21 10:00:04 +0000

    devel,.pylintrc, setup.py: Add pylint to standard check.
    * Disable run for squeeze (will not work for us)
    * Quit checking only errors, Disable all currently failing
      checks in rc file (should be phased out one by one)
    * This also fixes style errors in setup.py, and adds this file to the checks.

--- setup.py
+++ setup.py
@@ -4,44 +4,47 @@ import sys
 import shutil
 import distutils.core
 import debian.changelog
-from setuptools import setup
-from doc import apidoc
+import setuptools
+import doc.apidoc
+
+print "I: Using setuptools: {v}".format(v=setuptools.__version__)
+
 
 def sphinx_build_workaround():
-    sphinx_build_dir = "build/sphinx"
-    sphinx_static_files_build_dir = sphinx_build_dir + "/_static"
-    sphinx_static_files_source_dir = "doc/_static"
-    sphinx_template_files_build_dir = sphinx_build_dir + "/_templates"
-    sphinx_template_files_source_dir = "doc/_templates"
+    build_dir = "build/sphinx"
+    static_files_build_dir = build_dir + "/_static"
+    static_files_source_dir = "doc/_static"
+    template_files_build_dir = build_dir + "/_templates"
+    template_files_source_dir = "doc/_templates"
 
-    if not os.path.exists(sphinx_static_files_build_dir):
-        os.makedirs(sphinx_static_files_build_dir)
+    if not os.path.exists(static_files_build_dir):
+        os.makedirs(static_files_build_dir)
 
-    if not os.path.exists(sphinx_template_files_build_dir):
-        os.makedirs(sphinx_template_files_build_dir)
+    if not os.path.exists(template_files_build_dir):
+        os.makedirs(template_files_build_dir)
 
     # copy static files (shutil.copytree does not fit our needs)
-    #file_list = os.listdir(sphinx_static_files_source_dir)
+    #file_list = os.listdir(static_files_source_dir)
     #for f in file_list:
-    #    shutil.copy(sphinx_static_files_source_dir + "/" + f, sphinx_static_files_build_dir)
+    #    shutil.copy(static_files_source_dir + "/" + f, static_files_build_dir)
 
     # copy template files
-    file_list = os.listdir(sphinx_template_files_source_dir)
+    file_list = os.listdir(template_files_source_dir)
     for f in file_list:
-        shutil.copy(sphinx_template_files_source_dir + "/" + f, sphinx_template_files_build_dir)
+        shutil.copy(template_files_source_dir + "/" + f, template_files_build_dir)
 
     # copy main files
-    shutil.copy("doc/conf.py", sphinx_build_dir)
-    shutil.copy("doc/index.rst", sphinx_build_dir)
+    shutil.copy("doc/conf.py", build_dir)
+    shutil.copy("doc/index.rst", build_dir)
 
     # call local apidoc script (sphinx < 1.1)
     apidoc_arguments = ['doc/apidoc.py', '--force', '--output-dir', 'build/sphinx', 'mini_buildd']
-    apidoc.main(apidoc_arguments)
+    doc.apidoc.main(apidoc_arguments)
 
 # This is a Debian native package, the version is in
 # debian/changelog and nowhere else. We automagically get the
 # version from there, and update the mini_buildd package's
-# __version__.py
+# __init__.py
 __version__ = str(debian.changelog.Changelog(file=open("./debian/changelog", "rb")).version)
 with open("./mini_buildd/__init__.py", "wb") as version_py:
     version_py.write("""\
@@ -54,17 +57,17 @@ if "build_sphinx" in sys.argv:
     sphinx_build_workaround()
 
 distutils.core.setup(
-    name = "mini-buildd",
-    version = __version__,
-    description = "Mini Debian build daemon",
-    author = "Stephan Sürken",
-    author_email = "absurd@debian.org",
-    scripts = ["mini-buildd"],
-    packages = ["mini_buildd"],
-    package_data = {"mini_buildd": ["templates/mini_buildd/*.html",
-                                    "templates/admin/*.html",
-                                    "templatetags/*.py",
-                                    "fixtures/*.json",
-                                    "static/css/*.css",
-                                    "static/images/*.png",
-                                    "static/*.*"]})
+    name="mini-buildd",
+    version=__version__,
+    description="Mini Debian build daemon",
+    author="Stephan Sürken",
+    author_email="absurd@debian.org",
+    scripts=["mini-buildd"],
+    packages=["mini_buildd"],
+    package_data={"mini_buildd": ["templates/mini_buildd/*.html",
+                                  "templates/admin/*.html",
+                                  "templatetags/*.py",
+                                  "fixtures/*.json",
+                                  "static/css/*.css",
+                                  "static/images/*.png",
+                                  "static/*.*"]})

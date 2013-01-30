272dd60439f1391b52ecbd57947b902c16c77d3c
Stephan Sürken <absurd@debian.org>
2013-01-30 14:42:46 +0000

    reprepro: Avoid parallel reprepro calls (Fixes possible locking errors).

--- mini_buildd/reprepro.py
+++ mini_buildd/reprepro.py
@@ -6,6 +6,7 @@ from __future__ import unicode_literals
 
 import os
 import shutil
+import threading
 
 import logging
 
@@ -13,34 +14,62 @@ import mini_buildd.misc
 
 LOG = logging.getLogger(__name__)
 
+_LOCKS_LOCK = threading.Lock()
+_LOCKS = {}
+
 
 class Reprepro():
+    """
+    Abstraction to reprepro repository commands.
+
+    Locking
+    =======
+    This implicitly provides a locking mechanism to avoid
+    parallel calls to the same repository from mini-buildd
+    itself. This rules out any failed call due to reprepro
+    locking errors in the first place.
+
+    For the case that someone else is using reprepro
+    manually, we also always run it with '--waitforlock'.
+    """
     def __init__(self, basedir):
         self._basedir = basedir
-        self._cmd = ["reprepro", "--verbose", "--basedir={b}".format(b=basedir)]
+        self._cmd = ["reprepro", "--verbose", "--waitforlock=10", "--basedir={b}".format(b=basedir)]
+        # Seems dict.setdefault 'should' be atomic, but it may be not the case in all versions >=2.6
+        # See: http://bugs.python.org/issue13521
+        with _LOCKS_LOCK:
+            self._lock = _LOCKS.setdefault(self._basedir, threading.Lock())
+            LOG.debug("Lock for reprepro repository '{r}': {o}".format(r=self._basedir, o=self._lock))
+
+    def _call(self, args):
+        return mini_buildd.misc.sose_call(self._cmd + args)
+
+    def _call_locked(self, args):
+        with self._lock:
+            return self._call(args)
 
     def reindex(self):
-        # Update reprepro dbs, and delete any packages no longer in dists.
-        mini_buildd.misc.call(self._cmd + ["--delete", "clearvanished"])
+        with self._lock:
+            # Update reprepro dbs, and delete any packages no longer in dists.
+            self._call(["--delete", "clearvanished"])
 
-        # Purge all indices under 'dists/' (clearvanished does not remove indices of vanished distributions)
-        shutil.rmtree(os.path.join(self._basedir, "dists"), ignore_errors=True)
+            # Purge all indices under 'dists/' (clearvanished does not remove indices of vanished distributions)
+            shutil.rmtree(os.path.join(self._basedir, "dists"), ignore_errors=True)
 
-        # Finally, rebuild all indices
-        mini_buildd.misc.call(self._cmd + ["export"])
+            # Finally, rebuild all indices
+            self._call(["export"])
 
     def check(self):
-        mini_buildd.misc.call(self._cmd + ["check"])
+        self._call_locked(["check"])
 
     def list(self, pattern, distribution, typ=None, list_max=50):
         result = []
-        for item in mini_buildd.misc.call(self._cmd +
-                                          ["--list-format=${package}|${$type}|${architecture}|${version}|${$source}|${$sourceversion}|${$codename}|${$component};",
-                                           "--list-max={m}".format(m=list_max)] +
-                                          (["--type={t}".format(t=typ)] if typ else []) +
-                                          ["listmatched",
-                                           distribution,
-                                           pattern]).split(";"):
+        for item in self._call_locked(["--list-format=${package}|${$type}|${architecture}|${version}|${$source}|${$sourceversion}|${$codename}|${$component};",
+                                       "--list-max={m}".format(m=list_max)] +
+                                      (["--type={t}".format(t=typ)] if typ else []) +
+                                      ["listmatched",
+                                       distribution,
+                                       pattern]).split(";"):
             if item:
                 item_split = item.split("|")
                 result.append({"package": item_split[0],
@@ -57,10 +86,9 @@ class Reprepro():
     def show(self, package):
         result = []
         # reprepro ls format: "${$source} | ${$sourceversion} |    ${$codename} | source\n"
-        for item in mini_buildd.misc.call(self._cmd +
-                                          ["--type=dsc",
-                                           "ls",
-                                           package]).split("\n"):
+        for item in self._call_locked(["--type=dsc",
+                                       "ls",
+                                       package]).split("\n"):
             LOG.debug("ls: {i}".format(i=item))
             if item:
                 item_split = item.split("|")
@@ -71,10 +99,10 @@ class Reprepro():
         return result
 
     def migrate(self, package, src_distribution, dst_distribution, version=None):
-        return mini_buildd.misc.sose_call(self._cmd + ["copysrc", dst_distribution, src_distribution, package] + ([version] if version else []))
+        return self._call_locked(["copysrc", dst_distribution, src_distribution, package] + ([version] if version else []))
 
     def remove(self, package, distribution, version=None):
-        return mini_buildd.misc.sose_call(self._cmd + ["removesrc", distribution, package] + ([version] if version else []))
+        return self._call_locked(["removesrc", distribution, package] + ([version] if version else []))
 
     def install(self, changes, distribution):
-        return mini_buildd.misc.sose_call(self._cmd + ["include", distribution, changes])
+        return self._call_locked(["include", distribution, changes])

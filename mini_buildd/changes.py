SwooshyCueb/mini-buildd

commit 18730f72fc013142f8cd1b2fc5119de5a7944fda

logs: Only archive changes and buildlogs for "installed" packages.

mini_buildd/changes.py
@@ -247,9 +247,12 @@ class Changes(debian.deb822.Changes):
             os.makedirs(logdir)
         LOG.info("Moving changes to log: '{f}'->'{l}'".format(f=self._file_path, l=logdir))
         for fd in [{"name": self._file_name}] + self.get_files():
-            f = os.path.join(os.path.dirname(self._file_path), fd["name"])
-            LOG.debug("Moving: '{f}' to '{d}'". format(f=fd["name"], d=logdir))
-            os.rename(f, os.path.join(logdir, fd["name"]))
+            # If not installed, just save all files.
+            # If installed, only save buildlogs and changes.
+            if not installed or re.match(r"(.*\.buildlog$|.*changes$)", fd["name"]):
+                f = os.path.join(os.path.dirname(self._file_path), fd["name"])
+                LOG.debug("Moving: '{f}' to '{d}'". format(f=fd["name"], d=logdir))
+                os.rename(f, os.path.join(logdir, fd["name"]))
 
     def remove(self):
         LOG.info("Removing changes: '{f}'".format(f=self._file_path))

commit 1685ac181edf6b822e6bb9c10c6c587e0c41d255

changes: Fix: Some files not deleted from incoming (regression introduced with [18730f7]).

mini_buildd/changes.py
@@ -268,12 +268,16 @@ class Changes(debian.deb822.Changes):
             os.makedirs(logdir)
         LOG.info("Moving changes to log: '{f}'->'{l}'".format(f=self._file_path, l=logdir))
         for fd in [{"name": self._file_name}] + self.get_files():
-            # If not installed, just save all files.
+            f = fd["name"]
+            f_abs = os.path.join(os.path.dirname(self._file_path), f)
+            # If not installed, just move all files to log dir.
             # If installed, only save buildlogs and changes.
-            if not installed or re.match(r"(.*\.buildlog$|.*changes$)", fd["name"]):
-                f = os.path.join(os.path.dirname(self._file_path), fd["name"])
-                LOG.debug("Moving: '{f}' to '{d}'". format(f=fd["name"], d=logdir))
-                os.rename(f, os.path.join(logdir, fd["name"]))
+            if not installed or re.match(r"(.*\.buildlog$|.*changes$)", f):
+                LOG.info("Moving '{f}' to '{d}'". format(f=f, d=logdir))
+                os.rename(f_abs, os.path.join(logdir, f))
+            else:
+                LOG.info("Removing '{f}'". format(f=f))
+                os.remove(f_abs)
 
     def remove(self):
         LOG.info("Removing changes: '{f}'".format(f=self._file_path))